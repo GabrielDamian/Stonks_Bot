98797eb5558b963706a36e5506be538042d8adb3
GabrielDamian <damiangabriel54@yahoo.com>
2021-12-11 14:43:08 -0800

    Fixes la durere (generareVariatii + normalizare Segment Baza + variatii). Inceput compresie + expandare.

--- generatorCombinatiiSegmente.py
+++ generatorCombinatiiSegmente.py
@@ -30,6 +30,7 @@ class generatorSegment:
     inputData = None
 
     data = {
+        'segment': None,
         'size_segment_principal': None,
         'min_stretching': None,
         'max_stretching': None,
@@ -42,12 +43,14 @@ class generatorSegment:
         self.inputData = newData
         self.truncateInputData(1)
 
-    def setParams(self,new_size, new_min, new_max):
+    def setParams(self,new_size, new_min, new_max,segment):
+        self.data['segment'] = segment
         self.data['size_segment_principal'] = new_size
         self.data['min_stretching'] = new_min
         self.data['max_stretching'] = new_max
 
     def printData(self):
+        print('Segment:',self.data['segment'])
         print('Input data:',self.inputData)
         print(f'Size segment principal:{self.data["size_segment_principal"]}')
         print(f'Min streching:{self.data["min_stretching"]}')
@@ -57,7 +60,44 @@ class generatorSegment:
         temp =[[round(a[0],decimals), round(a[1],decimals)] for a in self.inputData]
         self.inputData = temp
 
+    def normalizeazaSegmentBaza(self):
+        #aduce x si y cu 0 ca punct de start
+
+        if self.data['segment'] == None:
+            raise 'Nu pot nromaliza, segment de baza nu a fost initializat (inca este None)!!!!'
+        segment = self.data['segment']
+
+        x_coords = []
+        y_coords = []
+        min_y_coords = segment[0][1] #primul y
+
+        for x in segment:
+            x_coords.append(x[0])
+            y_coords.append(x[1])
+
+        min_x = min(x_coords)
+        min_y = min(y_coords)
+
+        x_coords_normalizate = []
+        y_coords_normalizate = []
+
+        for x in x_coords:
+            x_coords_normalizate.append(x - min_x)
+        for x in y_coords:
+            y_coords_normalizate.append(x - min_y)
+
+        final = []
+        for index, a in enumerate(x_coords_normalizate):
+            final.append([a,y_coords_normalizate[index]])
+
+        self.data['segment'] = final
+
+
+
+
     def determinaSizeVariatii(self):
+        #toate valorile dintre min si max si le pune drept key in 'variatii'
+
         min = self.data['min_stretching']
         max = self.data['max_stretching']
 
@@ -67,13 +107,13 @@ class generatorSegment:
             sizes.append(index)
             index +=1
         for x in sizes:
-            self.data['variatii'][x] = []
+            self.data['variatii'][f'{x}'] = []
 
 
 
     def genereazaVariatii(self):
 
-        for variatie in self.data['variatii']:
+        for variatie in self.data["variatii"]:
             size_segment = int(variatie)
 
             index_global = 0 #parcurge tot graful pe un an
@@ -103,10 +143,64 @@ class generatorSegment:
             for y in variatii[x]:
                 print(y)
 
+    def normalizeazaVariatii(self):
+        variatii = self.data["variatii"]
+
+        new_variatii = { }
+        for x in variatii:
+            new_variatii[x] =[]
+            print('Normalizez variatia:',x)
+            for index, y in enumerate(variatii[x]):
+
+                values = y['values']
+                future_price = y['future_price']
+
+
+                #normalizez vectorul values
+                x_coords = []
+                y_coords = []
+
+                for a in values:
+                    x_coords.append(a[0])
+                    y_coords.append(a[1])
+
+                min_x = min(x_coords)
+                min_y = min(y_coords)
+
+                x_coords_normalized = []
+                y_coords_normalized = []
+
+                for a in x_coords:
+                    x_coords_normalized.append(round(a - min_x,1))
+                for a in y_coords:
+                    y_coords_normalized.append(round(a - min_y,1))
+
+                new_values =[]
+                for index, a in enumerate(x_coords_normalized):
+                    new_values.append([a,y_coords_normalized[index]])
+
+
+                new_future_price = []
+                if future_price != None:
+                    new_future_price.append(round(future_price[0] - min_x,1))
+                    new_future_price.append(round(future_price[1] - min_y,1))
+                else:
+                    new_future_price = future_price
+
+
+                new_variatii[x].append({
+                    'values': new_values,
+                    'future_price': new_future_price
+                })
+
+        self.data['variatii'] = new_variatii
+
+
+
     def comprimare_segmente_mari(self):
         segmente = [x for x in self.data['variatii']]
 
-        #filtreza doar segmentele mari
+        #lasa aici doar segmentele mari
         segmente_mari = [x for x in segmente if x > self.data['size_segment_principal']]
 
         for x in segmente_mari:
@@ -119,4 +213,5 @@ class generatorSegment:
 
                 #vr sa comprim current_big_arr de la from_size la to_size (in final o sa aiba to_size puncte)
 
-
+    def comprimare_segmente_mici(self):
+        pass
--- main.py
+++ main.py
@@ -34,20 +34,36 @@ if __name__ == '__main__':
 
 
     #GENERATOR COMBINATII SEGMENTE
-    generatorCombinatii = generatorSegment(graph.candlesToFunction)
-    generatorCombinatii.setParams(5,2,7)
 
+    #constructorul primeste inputData si il seteaza pe variabila
+    generatorCombinatii = generatorSegment(graph.candlesToFunction)
+
+    #data temporara, patternFinder o sa incerc orice segment(shiftat la dreapta cu o unitate)
+    segment_curent_fals = [[5,28609],[6,28620],[7,28631],[8,28643],[9,28654]]
+
+    #seteaza parametrii obiectului (len(segment_curent_fals)),min_strech, max_strech, segment_curent)
+    generatorCombinatii.setParams(5,2,7,segment_curent_fals)
+
+    #forteaza segment_curent sa inceapa din 0 (atat x cat si y)
+    generatorCombinatii.normalizeazaSegmentBaza()
+
+    #insereaza cate o cheie in 'variatii' pentru fiecare unitate intre min_strech si max_strech
     generatorCombinatii.determinaSizeVariatii()
+
+    #parcurge cheile variatiilor si segmenteaza toate segmentele posibile de acea dimensiune
     generatorCombinatii.genereazaVariatii()
+
     # generatorCombinatii.printData()
 
+    #aduce toate variatiile in 0,0 ca punct de start (nu afecteaza marimea variatiei inca)
+    generatorCombinatii.normalizeazaVariatii()
 
     generatorCombinatii.printVariatii()
-    generatorCombinatii.comprimare_segmente_mari()
 
+    #comprima segmentele mai mari decat segmentul de baza la marimea segmentului de baza (Atat pe ox si cat pe oy)
+    # generatorCombinatii.comprimare_segmente_mari()
 
-
-
+    # generatorCombinatii.comprimare_segmente_mici()
 
 
     # plt.show()
